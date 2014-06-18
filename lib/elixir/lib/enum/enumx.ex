298fe17fe00f306f840453bd228c5095060295e9
Keisuke Takahashi <keithseahus@gmail.com>
2014-06-19 00:12:31 +0900

    Modify examples for EnumX.group_by/4

--- lib/elixir/lib/enum/enumx.ex
+++ lib/elixir/lib/enum/enumx.ex
@@ -62,9 +62,9 @@ defmodule EnumX do
 
   ## Examples
 
-      iex> EnumX.group_by(~w{ant buffalo cat dingo}, %{}, &String.length/1, &String.capitalize/1)
+      iex> EnumX.group_by(~w{ant buffalo cat dingo}, &String.length/1, &String.capitalize/1)
       %{3 => ["Cat", "Ant"], 5 => ["Dingo"], 7 => ["Buffalo"]}
-      iex> EnumX.group_by([a: 1, b: 0, c: 4, d: 0, e: 1, f: 1], %{}, fn({_, v}) -> v end, fn({k, _}) -> to_string(k) end)
+      iex> EnumX.group_by([a: 1, b: 0, c: 4, d: 0, e: 1, f: 1], fn({_, v}) -> v end, fn({k, _}) -> to_string(k) end)
       %{0 => ["d", "b"], 1 => ["f", "e", "a"], 4 => ["c"]}
 
   """
