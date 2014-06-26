1c544ec5ae7327862cffce856ea678c268162725
Keisuke Takahashi <keithseahus@gmail.com>
2014-06-26 09:10:13 +0900

    Introduce MathX.remainder/2

--- lib/elixir/lib/math/mathx.ex
+++ lib/elixir/lib/math/mathx.ex
@@ -22,4 +22,18 @@ defmodule MathX do
     num
   end
 
+  @doc """
+  Return the remainder value of 2nd argument into 1st argument.
+
+  ## Examples
+
+      iex> MathX.remainder(79, 31)
+      17
+
+  """
+  @spec remainder(number, number) :: number
+  def remainder(dividend, divisor) when is_number(dividend) and is_number(divisor) do
+    dividend - Float.floor(dividend / divisor) * divisor |> absolute
+  end
+
 end
